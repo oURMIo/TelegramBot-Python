d2f5451699c7131196e4302057234bcd272cdb0e
Dmitry Chistyakov <dchistyakov@reksoft.ru>
2023-10-17 12:07:24 +0400

    Add config template

---
+++ pythonBot/conf.py
@@ -0,0 +1,22 @@
+# TOKEN BOT
+TOKEN = " "
+
+# SERVER URLs
+URL_DACHSERV = " "
+URL_CHSERV = " "
+
+# PROJECT URLs
+URL_PROJECT_CHECK = " "
+URL_PROJECT_MORSE = " "
+
+# TOOLS URLs
+URL_TOOL_DOMAIN = " "
+URL_TOOL_DRIVE = " "
+
+# FLOOD
+LIST_OF_FUNCTIONS = """help - List of possible commands
+projects - List of my projects
+instruments - List of miscellaneous tools
+for_family       - List urls for chistyakovs
+check_dachserv - Status check dachserv
+check_enable  - Work = true"""
