3644ea668207d839c786391d24a300bec7afcbe2
Dmitry Chistyakov <dchistyakov@reksoft.ru>
2023-10-17 19:53:00 +0400

    Edit conf

--- pythonBot/conf.py
+++ pythonBot/conf.py
@@ -1,5 +1,8 @@
 # TOKEN BOT
-TOKEN = "  "
+TOKEN = " "
+
+# ADMIN ID
+ADMIN_ID = " "
 
 # SERVER URLs
 URL_DACHSERV = " "
@@ -14,9 +17,12 @@ URL_TOOL_DOMAIN = " "
 URL_TOOL_DRIVE = " "
 
 # FLOOD
-LIST_OF_FUNCTIONS = """help - List of possible commands
+LIST_OF_FUNCTIONS = """
+help - List of possible commands
 projects - List of my projects
 instruments - List of miscellaneous tools
-for_family       - List urls for chistyakovs
+for_family - List urls for chistyakovs
+subscribe - Subscription to notifications
+unsubscribe - Unsubscribe from notifications
 check_dachserv - Status check dachserv
 check_enable  - Work = true"""
