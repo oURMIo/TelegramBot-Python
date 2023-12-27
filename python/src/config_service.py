effeb6d4b5abf74332b20e3b6e0f4b4f204c628e
Dmitry Chistyakov <dchistyakov@reksoft.ru>
2023-12-27 16:38:58 +0400

    Finished refactoring

---
+++ python/src/config_service.py
@@ -0,0 +1,40 @@
+import configparser
+from pathlib import Path
+
+class ConfigService:
+    def __init__(self, file_path='config/config.ini'):
+        realpath = Path(file_path).resolve()
+        self.file_path = realpath
+        try:
+            self.config = configparser.ConfigParser()
+            self.config.read(realpath)
+        except FileNotFoundError:
+            print(f"Config file not found: {realpath}")
+            self.config = None
+
+    def get_token(self):
+        return str(self.config.get('TOKEN', 'BOT_TOKEN', fallback=None)) if self.config else None
+
+    def get_admin_id(self):
+        return self.config.get('ADMIN_ID', 'MAIN_ADMIN_ID', fallback=None) if self.config else None
+
+    def get_url_dachserv(self):
+        return self.config.get('SERVER_URLS', 'URL_DACHSERV', fallback=None) if self.config else None
+
+    def get_url_chserv(self):
+        return self.config.get('SERVER_URLS', 'URL_CHSERV', fallback=None) if self.config else None
+
+    def get_check_dachserv_url(self):
+        return self.config.get('CHECK_URLS', 'CHECK_DACHSERV_URL', fallback=None) if self.config else None
+    
+    def get_check_chserv_url(self):
+        return self.config.get('CHECK_URLS', 'CHECK_CHSERV_URL', fallback=None) if self.config else None
+
+    def get_url_project_morse(self):
+        return self.config.get('PROJECT_URLS', 'URL_PROJECT_MORSE', fallback=None) if self.config else None
+
+    def get_url_tool_domain(self):
+        return self.config.get('TOOLS_URLS', 'URL_TOOL_DOMAIN', fallback=None) if self.config else None
+
+    def get_url_tool_drive(self):
+        return str(self.config.get('TOOLS_URLS', 'URL_TOOL_DRIVE', fallback=None)) if self.config else None
